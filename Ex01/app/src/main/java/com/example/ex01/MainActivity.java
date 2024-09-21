b748e85067c3e7588f2885a7c2923696ac848706
znhungz <hongnhung7750@gmail.com>
2024-09-22 00:20:23 +0700

    anh xa cac bien duoc khoi tao voi cac id o file giao dien xml

--- Ex01/app/src/main/java/com/example/ex01/MainActivity.java
+++ Ex01/app/src/main/java/com/example/ex01/MainActivity.java
@@ -13,5 +13,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtA,edtB,edtKQ;
+    Button btntong;
 
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //Anh xa ID cho cac bien giao dien
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btntong = findViewById(R.id.btntong);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
