25658f7d00c74a862106ff401ed666713e836bd7
znhungz <hongnhung7750@gmail.com>
2024-10-10 15:14:04 +0700

    Khoi tao du an

---
+++ Ex03/app/src/main/java/com/example/ex03/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ex03;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends Activity {
+
+}
+
