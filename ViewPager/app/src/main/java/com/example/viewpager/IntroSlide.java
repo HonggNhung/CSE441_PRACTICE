4fee3b33ea04c8524c4d7b532b8a7bdefb334f83
znhungz <hongnhung7750@gmail.com>
2024-10-14 14:22:28 +0700

    ViewPager

---
+++ ViewPager/app/src/main/java/com/example/viewpager/IntroSlide.java
@@ -0,0 +1,13 @@
+package com.example.viewpager;
+
+public class IntroSlide {
+    public int imageResId;
+    public String title;
+    public String description;
+
+    public IntroSlide(int imageResId, String title, String description) {
+        this.imageResId = imageResId;
+        this.title = title;
+        this.description = description;
+    }
+}
