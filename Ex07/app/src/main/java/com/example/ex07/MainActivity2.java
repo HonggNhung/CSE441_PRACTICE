b466932872434f554e8345269ecb4aacc2e908b8
znhungz <hongnhung7750@gmail.com>
2024-10-17 02:22:52 +0700

    Main_Child_Activity

--- Ex07/app/src/main/java/com/example/ex07/MainActivity2.java
+++ Ex07/app/src/main/java/com/example/ex07/MainActivity2.java
@@ -2,8 +2,8 @@ package com.example.ex07;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,20 +11,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
-            }
+        setContentView(R.layout.activity_main2);
+        Button btn = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+        btn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+            startActivity(intent);
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
